9b0b6a7d61e7de10b47183f1cdbaba3e62fce01e
Olexiy Zamkoviy <olexiy.z@gmail.com>
2014-05-05 11:16:34 +0300

    Schema dump improvement, sorting models and fields during dump

--- model-description.lisp
+++ model-description.lisp
@@ -5,11 +5,28 @@
    (title :initarg :title :accessor model-description-title)
    (name  :initarg :name :accessor model-description-name :type keyword)))
 
+(defun string<-by-chars (str1 str2)
+  (cond 
+    ((or 
+       (zerop (length str1))
+       (zerop (length str2)))
+     (> (length str1) (length str2)))
+    ((char= (char str1 0) (char str2 0))
+     (string<-by-chars (subseq str1 1) (subseq str2 1)))
+    ((char< (char str1 0) (char str2 0)) t)))
+
+(assert (equal '("Test" "Test") (sort (list "Test" "Test") #'string<-by-chars)))
+(assert (equal '("2" "") (sort (list "" "2" ) #'string<-by-chars)))
+(assert (equal '("Test2" "Test") (sort (list "Test" "Test2" ) #'string<-by-chars)))
+
 (defmethod dump-model-description ((model model-description))
   (list :title (model-description-title model)
         :name (model-description-name model)
         :fields 
-        (loop for i in (find-by-values 'field-description :model model)
+        (loop for i in (sort 
+                         (find-by-values 'field-description :model model)
+                         #'string<-by-chars
+                         :key #'field-description-title)
               collect (dump-field-description i))))
 
 (defvar *schema-file* (merge-pathnames 
@@ -23,7 +40,10 @@
             (remove-if 
               (lambda (item)
                 (find (model-description-name item) disabled-names))
-              (all-of 'model-description)))))
+              (sort 
+                (all-of 'model-description)
+                #'string<-by-chars 
+                :key #'model-description-title)))))
 
 (defun save-schema (&optional (file *schema-file*))
   (with-open-file 
